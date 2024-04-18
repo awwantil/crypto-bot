bd93b0b958e4b55775c6781df38d891a3a5698b9
vpomo <pomogalov@gmail.com>
2024-04-18 19:07:36 +0300

    updated base connect to db

--- frontend-service/models/base.go
+++ frontend-service/models/base.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -19,33 +20,24 @@ var db *gorm.DB
 func ConnectDB() {
 	blogger.Infoln("Connecting to DB")
 
-	//e := godotenv.Load()
-	//if e != nil {
-	//	fmt.Print(e)
-	//}
-
-	logger.Info("***** 4-4 *****")
+	logger.Info("***** 5-5 *****")
 
 	dbHost := os.Getenv("DB_HOST")
-	logger.Info("dbHost", dbHost)
+	logger.Info("dbHost: ", dbHost)
 
-	dbUser := os.Getenv("DB_USER")
-	logger.Info("dbUser", dbUser)
-
-	dbPassword := os.Getenv("DB_PASSWORD")
-	logger.Info("dbPassword", dbPassword)
-
-	dbName := os.Getenv("DB_NAME")
-	logger.Info("dbName", dbName)
+	if dbHost == "" {
+		err := godotenv.Load()
+		if err != nil {
+			blogger.Errorf("Wrong load .env file: %s", err)
+		}
+	}
 
 	tokenPassword := os.Getenv("TOKEN_PASSWORD")
-	logger.Info("tokenPassword", tokenPassword)
-
-	nodeEnv := os.Getenv("NODE_ENV")
-	logger.Info("nodeEnv", nodeEnv)
+	logger.Info("tokenPassword: ", tokenPassword)
 
 	dsn := fmt.Sprintf(
-		"host=185.240.103.204 user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
